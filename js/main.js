f422975df421ca884f6013a35ba386ee5d4ece78
Pratyaksh Prajapati <pratykash@gmail.com>
2020-10-24 22:01:48 +0530

    V1

---
+++ js/main.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// custom console
+var myConsole = {
+	log: function(text) {
+		document.getElementById('myConsoleRenderArea').value += text + '\n';
+	}
+};
+
+// init blockly
+var blocklyArea = document.getElementById('blocklyArea');
+var workspace = Blockly.inject(blocklyArea, {
+	oneBasedIndex: false,
+	toolbox: document.getElementById('toolbox')
+});
+var resetButton = document.getElementById('resetButton');
+var playButton = document.getElementById('playButton');
+var clearButton = document.getElementById('clearButton');
+
+Blockly.svgResize(workspace);
+
+// auto render code
+function renderCode(event) {
+	var code = Blockly.JavaScript.workspaceToCode(workspace);
+	document.getElementById('codeRenderArea').value = code;
+}
+
+workspace.addChangeListener(renderCode);
+
+// run code
+function evalCode(event) {
+	var code = document.getElementById('codeRenderArea').value;
+
+	try {
+		window.eval(code);
+	} catch (e) {
+		myConsole.log(e.name + ': ' + e.message);
+	}
+}
+
+playButton.addEventListener('click', evalCode, false);
+
+// clear console
+function clearConsole(event) {
+	document.getElementById('myConsoleRenderArea').value = '';
+}
+
+clearButton.addEventListener('click', clearConsole, false);
+
+// auto-backup and restore blocks with local storage
+BlocklyStorage.backupOnUnload();
+window.setTimeout(BlocklyStorage.restoreBlocks, 0);
